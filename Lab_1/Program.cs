c87775ec2ac23b1ff37adf6a86bdd1349156fea3
Денис Трепаков <naitgames@mail.ru>
2025-09-04 22:50:38 +0700

    End Commit for 1`st Lab

--- Lab_1/Program.cs
+++ Lab_1/Program.cs
@@ -3,13 +3,24 @@
 // Считать число и отнять от него 1
 // Вывести результат 
 
-int Factorial(int a)
+Console.WriteLine("Введите число для расчета факториала:");
+int a = Convert.ToInt32(Console.ReadLine());
+
+void Factorial(int number)
 {
-    if (a == 0 || a == 1)
-        return 1;
-    else
+    // Проверка отрицательного числа 
+    if (number <= 0)
     {
-        return a * Factorial(a - 1);
+        Console.WriteLine("Факториал не может быть равен 0 или отрицательным");
+        return;
     }
-} 
-Console.WriteLine(Factorial(5));
+
+    int result = 1;
+    for (int i = 1; i <= number; i++)
+    {
+        result *= i;
+    }
+    Console.WriteLine($"Факториал {number} = {result}");
+}
+
+Factorial(a);
