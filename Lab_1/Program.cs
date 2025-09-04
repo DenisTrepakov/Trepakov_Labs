8ac2bb14874b3d357994cb99bf8365d85ecd8408
Денис Трепаков <naitgames@mail.ru>
2025-09-04 21:22:42 +0700

    Small commit with few features

--- Lab_1/Program.cs
+++ Lab_1/Program.cs
@@ -1,2 +1,15 @@
-﻿Console.WriteLine("Hello, World!");
-Console.WriteLine("Welcome to the My Labs");
+﻿// Факториал числа n
+// Ввести число 
+// Считать число и отнять от него 1
+// Вывести результат 
+
+int Factorial(int a)
+{
+    if (a == 0 || a == 1)
+        return 1;
+    else
+    {
+        return a * Factorial(a - 1);
+    }
+} 
+Console.WriteLine(Factorial(5));
