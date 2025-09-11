784b970bfbc808272c2659f9913aca7d13e355ab
Денис Трепаков <naitgames@mail.ru>
2025-09-11 09:43:58 +0700

    3

--- Lab_1/Program.cs
+++ Lab_1/Program.cs
@@ -1,29 +1,60 @@
-﻿// Факториал числа n
-// Ввести число 
-// Считать число и отнять от него 1
-// Вывести результат 
+namespace Trepakov_Labs.Lab_1;
 
-Console.WriteLine("Введите число для расчета факториала:");
-int a = Convert.ToInt32(Console.ReadLine());
+using System;
 
-void Factorial(int number)
+public class Program
 {
-    // Проверка отрицательного числа 
-    if (number <= 0)
+    public static void Main(string[] args)
     {
-        Console.WriteLine("Факториал не может быть равен 0 или отрицательным");
-        return;
-    }
+        // 1 Задание
+        void first()
+        {
+            Console.WriteLine("Введите число для расчета факториала:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Factorial(a);
+        }
+        
+        // Локальная функция — без модификатора доступа
+        void Factorial(int number)
+        {
+            if (number <= 0)
+            {
+                Console.WriteLine("Факториал не может быть равен 0 или отрицательным");
+                return;
+            }
 
-    int result = 1;
-    for (int i = 1; i <= number; i++)
-    {
-        result *= i;
+            int result = 1;
+            for (int i = 1; i <= number; i++)
+            {
+                result *= i;
+            }
+            Console.WriteLine($"Факториал {number} = {result}");
+        }
+        
+        // 2 Задание
+        static double Calculate(double x)
+        {
+            double lnCalc = Math.Sqrt(Math.Log(4.0/3.0));
+            double linear = x + 9.0 / 7.0;
+            double exp = Math.Exp(Math.Sin(1.3 * x - 0.7));
+            
+            double term = lnCalc + linear - exp;
+            return term;
+        }
+
+        void Term2()
+        {
+            Console.WriteLine("Введите число для X:");
+            double x = Convert.ToDouble(Console.ReadLine());
+            
+            double result = Calculate(x);
+            Console.WriteLine($"Функция = {result}");
+        }
+        
+        
+        
+        // Вызов 
+        Term2();
+        //first();
     }
-    Console.WriteLine($"Факториал {number} = {result}");
 }
-
-Factorial(a);
-
-//hello world!
-Console.WriteLine("Привет, мир!");
